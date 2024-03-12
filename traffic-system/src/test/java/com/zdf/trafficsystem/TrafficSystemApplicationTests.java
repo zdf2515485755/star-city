72502eaed7d397fb5b34b510775596ee4760f04c
mrzhang <2515485755@qq.com>
2024-03-13 01:32:57 +0800

    feature:add log config file

--- traffic-system/src/test/java/com/zdf/trafficsystem/TrafficSystemApplicationTests.java
+++ traffic-system/src/test/java/com/zdf/trafficsystem/TrafficSystemApplicationTests.java
@@ -1,13 +1,17 @@
 package com.zdf.trafficsystem;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TrafficSystemApplicationTests {
 
+   Logger logger = LoggerFactory.getLogger(this.getClass());
     @Test
     void contextLoads() {
+        logger.error("test logger");
     }
 
 }
