0480ad0bdfc9b44167fe7c174c2b6b70e4384ad3
mrzhang <2515485755@qq.com>
2024-03-28 22:36:50 +0800

    feature:Implement the function of operating remote call

--- traffic-wms/src/main/java/com/zdf/trafficwms/TrafficWmsApplication.java
+++ traffic-wms/src/main/java/com/zdf/trafficwms/TrafficWmsApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author mrzhang
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.zdf.trafficwms.mapper")
+@EnableFeignClients
 public class TrafficWmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(TrafficWmsApplication.class, args);
